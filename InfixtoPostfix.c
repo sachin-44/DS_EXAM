0ccafeb357b62ba5286c560503311eeee9f3cddf
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-11-13 01:11:55 +0530

    Update InfixtoPostfix.c

--- InfixtoPostfix.c
+++ InfixtoPostfix.c
@@ -114,3 +114,25 @@ int main()
     infix2postfix(exp);
     return 0;
 }
+
+
+
+
+1. Push left parenthesis onto STACK and add right parenthesis at the end of Q.
+ 
+2. Scan Q from left to right and repeat step 3 to 6 for each element of Q until the STACK is empty.
+ 
+3. If an operand is encountered add it to P.
+ 
+4. If a left parenthesis is encountered push it onto the STACK.
+ 
+5. If an operator is encountered, then
+Repeatedly pop from STACK and add to P each operator
+which has same precedence as or higher precedence than the operator
+encountered.
+Push the encountered operator onto the STACK.
+6. If a right parenthesis is encountered, then
+Repeatedly pop from the STACK and add to P each operator
+until a left parenthesis is encountered.
+Remove the left parenthesis; do not add it to P.
+
