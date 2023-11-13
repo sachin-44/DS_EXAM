ebe660a6232d661b89760f0082cb9af6e4960dc1
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-11-13 07:22:02 +0530

    Create tupleaddtrans algorithm.c

---
+++ tupleaddtrans algorithm.c
@@ -0,0 +1,18 @@
+Define a structure named matrix with attributes row, col, and value.
+
+Create a function named input that takes a 2D array, number of rows, and number of columns as parameters and inputs the matrix elements.
+
+Create a function named conversion that takes a 2D array, number of rows, number of columns, and an array of structures as parameters. This function converts the matrix to a tuple form and returns the number of non-zero elements.
+
+Create a function named display that takes an array of structures and its size as parameters and displays the matrix in tuple form.
+
+Create a function named transpose that takes an array of structures, an array for the transposed matrix, and the number of non-zero elements. This function transposes the matrix and stores the result in the transposed array.
+Create a function named add that takes two arrays of structures representing matrices, their sizes, and an array for the result. This function performs matrix addition and returns the number of non-zero elements in the result.
+In the main function:
+Declare variables r1, c1, r2, c2, k, d, and v.
+Declare 2D arrays matA and matB for the matrices.
+Declare arrays of structures mat1, mat2, trans, and res for the tuple forms, transpositions, and result of matrices.
+Input matrices matA and matB.
+Convert matrices to tuple form and display them.
+Transpose matrices and display the result.
+Perform matrix addition and display the result.
