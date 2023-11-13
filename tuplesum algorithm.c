f15d2e367c3a3149964d9e23b1e6874a9ac815de
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-11-13 07:12:24 +0530

    Update tuplesum algorithm.c

--- tuplesum algorithm.c
+++ tuplesum algorithm.c
@@ -1,1 +1,23 @@
+Define a structure named matrix with attributes row, col, and value.
+
+Create a function named input that takes a 2D array, number of rows, and number of columns as parameters and inputs the matrix elements.
+
+Create a function named conversion that takes a 2D array, number of rows, number of columns, and an array of structures as parameters. This function converts the matrix to a tuple form and returns the number of non-zero elements.
+
+Create a function named display that takes an array of structures and its size as parameters and displays the matrix in tuple form.
+
+Create a function named add that takes two arrays of structures representing matrices, their sizes, and an array for the result. This function performs matrix addition and returns the number of non-zero elements in the result.
+
+In the main function:
+Declare variables r1, c1, r2, c2, v, and choice.
+Declare 2D arrays matA and matB for the matrices.
+Declare arrays of structures mat1, mat2, res for the tuple forms of matrices.
+Input matrices matA and matB.
+Convert matrices to tuple form and display them.
+Perform matrix addition and display the result.
+
+
+
+
+
 
