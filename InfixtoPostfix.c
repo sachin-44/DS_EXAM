7ca5ed06aec0ec6b307ec6e2cf35f561da706e41
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-12-17 13:26:53 +0530

    Update InfixtoPostfix.c

--- InfixtoPostfix.c
+++ InfixtoPostfix.c
@@ -1,118 +1,66 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
+#include<stdio.h>
+#include<ctype.h>
 
-char operatorStack[100];
+char stack[100];
 int top = -1;
-void OperatorStack_push(char ch)
+
+void push(char x)
 {
-    operatorStack[++top] = ch;
+    stack[++top] = x;
 }
 
-char OperatorStack_pop()
+char pop()
 {
-    return operatorStack[top--];
-}
-
-char OperatorStack_peek()
-{
-    return operatorStack[top];
-}
-
-bool OperatorStack_isEmpty()
-{
-    return top == -1;
-}
-
-int getPrecedence(char c)
-{
-    switch (c)
-    {
-    case '$':
-        return 4;
-    case '^':
-        return 3;
-    case '*':
-    case '/':
-        return 2;
-    case '+':
-    case '-':
-        return 1;
-    default:
+    if(top == -1)
         return -1;
-    }
+    else
+        return stack[top--];
 }
 
-bool hasHigherPrecedence(char c1, char c2)
+int priority(char x)
 {
-    return getPrecedence(c1) >= getPrecedence(c2);
-}
-
-bool isOperand(char c)
-{
-    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'));
-}
-
-bool isOpeningBracket(char c)
-{
-    return c == '(' || c == '{' || c == '[';
-}
-
-bool isClosingBracket(char c)
-{
-    return c == ')' || c == '}' || c == ']';
-}
-
-void infix2postfix(char exp[])
-{
-    int n = strlen(exp);
-    char postfix[100]; 
-    int k = 0;         
-    for (int i = 0; i < n; i++)
-    {
-        if (isOperand(exp[i])) 
-        {
-            postfix[k++] = exp[i]; 
-        }
-        else if (!isOpeningBracket(exp[i]) && !isClosingBracket(exp[i]))
-        {
-            while (!OperatorStack_isEmpty() && !isOpeningBracket(OperatorStack_peek()) && hasHigherPrecedence(OperatorStack_peek(), exp[i])) 
-            {
-                postfix[k++] = OperatorStack_pop(); 
-            }
-            OperatorStack_push(exp[i]); 
-        }
-        else if (isOpeningBracket(exp[i])) 
-        {
-            OperatorStack_push(exp[i]); 
-        }
-        else if (isClosingBracket(exp[i]))
-        {
-            while (!OperatorStack_isEmpty() && !isOpeningBracket(OperatorStack_peek())) 
-            {
-                postfix[k++] = OperatorStack_pop(); 
-            }
-            OperatorStack_pop(); 
-        }
-    }
-    while (!OperatorStack_isEmpty()) 
-    {
-        postfix[k++] = OperatorStack_pop();
-    }
-    printf("\nPostfix expression\n"); 
-    for (int i = 0; i < k; i++)
-    {
-        printf("%c", postfix[i]);
-    }
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    return 0;
 }
 
 int main()
 {
     char exp[100];
-    printf("\nEnter infix expression without any spaces");
-    scanf("%s", &exp);
-    infix2postfix(exp);
-    return 0;
+    char *e, x;
+    printf("Enter the expression : ");
+    scanf("%s",exp);
+    printf("\n");
+    e = exp;
+    
+    while(*e != '\0')
+    {
+        if(isalnum(*e))
+            printf("%c ",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')')
+        {
+            while((x = pop()) != '(')
+                printf("%c ", x);
+        }
+        else
+        {
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c ",pop());
+            push(*e);
+        }
+        e++;
+    }
+    
+    while(top != -1)
+    {
+        printf("%c ",pop());
+    }return 0;
 }
 
 
