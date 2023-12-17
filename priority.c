6f730575b08095ad6b03cf3c4503b97a6ce6a11e
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-12-17 13:34:38 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,119 +1,113 @@
+#include <stdio.h>
 
-#include<stdio.h>
-#include<stdlib.h>
+#define MAX_SIZE 10
 
-struct node
-{
-	int data,prio;
-	struct node *link;
-}; struct node *front=NULL;
+// Priority Queue structure
+struct PriorityQueue {
+    int elements[MAX_SIZE];
+    int priorities[MAX_SIZE];
+    int size;
+};
 
-void endq() ;
-void deq();
-void display();
+// Function prototypes
+void insertElement(struct PriorityQueue* pq, int element, int priority);
+void deleteElement(struct PriorityQueue* pq);
+void displayPriorityQueue(struct PriorityQueue* pq);
 
-int main()
-{
- int choice;
- 
- 
- 
-  
- while(1)
-	{
-	printf(" 1.endqueue\n 2.dequeue\n 3.display\n 4.exit\n Enter the choice:");
-	scanf("%d",&choice);
-	switch(choice)
-	{
-	  case 1:
-		
-		
-		endq();
-		break;
-	  case 2:
-		deq();
-		break;
-	  case 3:
-		display();
-		break;
-	  case 4:exit(0);
-	  default:printf("invalid choice\n");
-			break;
-		
-	}
-	}
-}
-//CSL201 DATA STRUCTURES LAB ----- DEION TOMSON
-void endq()
-{	int p;
-	struct node *new,*temp;
-	new=(struct node *)malloc(sizeof(struct node));
-	new->link=NULL;
-	printf("enter data:");
-	scanf("%d",&new->data);
-	printf("enter priority:");
-	scanf("%d",&p);
-	new->prio=p;
-	if(front==NULL)
-	{
-		front=new;
-	}
-	else if(p<(front->prio))
-	{
-		new->link=front;
-		front=new;
-	}
-	else
-	{
-		temp=front;
-		while(temp->link!=NULL)
-		{
-			if((temp->link->prio)<=p)
-				temp=temp->link;
-			else
-				break;
-		}
-		if(temp->link==NULL)
-		{
-			temp->link=new;
-		}
-		else
-			{new->link=temp->link;
-			temp->link=new;}
-	}
-	
+int main() {
+    struct PriorityQueue pq;
+    pq.size = 0;
+    int choice, element, priority;
+
+    do {
+        printf("\nPriority Queue Operations:\n");
+        printf("1. Insert Element\n");
+        printf("2. Delete Element\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                if (pq.size < MAX_SIZE) {
+                    printf("Enter the element to insert: ");
+                    scanf("%d", &element);
+                    printf("Enter the priority of the element: ");
+                    scanf("%d", &priority);
+                    insertElement(&pq, element, priority);
+                    displayPriorityQueue(&pq);
+                } else {
+                    printf("Priority Queue is full. Cannot insert element.\n");
+                }
+                break;
+
+            case 2:
+                if (pq.size > 0) {
+                    deleteElement(&pq);
+                    displayPriorityQueue(&pq);
+                } else {
+                    printf("Priority Queue is empty. Cannot delete element.\n");
+                }
+                break;
+
+            case 3:
+                displayPriorityQueue(&pq);
+                break;
+
+            case 4:
+                printf("Exiting the program.\n");
+                break;
+
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
 }
 
-void deq()
-{
-	struct node *temp;
-	if (front==NULL)
-    	printf("under flow\n");
- 
- 	
-     else
-	{
-		temp=front;
-		printf("dequed element:%d\n",temp->data);
-		front=front->link;
-		free(temp);
+// Function to insert an element into the Priority Queue
+void insertElement(struct PriorityQueue* pq, int element, int priority) {
+    int i;
 
-	}
+    // Find the position to insert the new element based on priority
+    for (i = pq->size - 1; i >= 0 && priority > pq->priorities[i]; i--) {
+        pq->elements[i + 1] = pq->elements[i];
+        pq->priorities[i + 1] = pq->priorities[i];
+    }
+
+    pq->elements[i + 1] = element;
+    pq->priorities[i + 1] = priority;
+    pq->size++;
+    printf("Element %d with priority %d inserted successfully.\n", element, priority);
 }
-void display()
-{
-		if(front==NULL)
-			printf("queue empty\n");
-		else
-			{
-				struct node *temp=front;
-				while(temp!= NULL)
-				{
-					printf("data :%d priority:%d\n",temp->data,temp->prio);
-					temp=temp->link;		
-				}
 
-			}
+// Function to delete an element from the Priority Queue
+void deleteElement(struct PriorityQueue* pq) {
+    int deletedElement = pq->elements[0];
+    int deletedPriority = pq->priorities[0];
 
+    // Shift the elements to fill the gap
+    for (int i = 0; i < pq->size - 1; i++) {
+        pq->elements[i] = pq->elements[i + 1];
+        pq->priorities[i] = pq->priorities[i + 1];
+    }
 
+    pq->size--;
+    printf("Deleted element %d with priority %d.\n", deletedElement, deletedPriority);
+}
+
+// Function to display the elements of the Priority Queue
+void displayPriorityQueue(struct PriorityQueue* pq) {
+    if (pq->size == 0) {
+        printf("Priority Queue is empty.\n");
+        return;
+    }
+
+    printf("Priority Queue: ");
+    for (int i = 0; i < pq->size; i++) {
+        printf("(%d, %d) ", pq->elements[i], pq->priorities[i]);
+    }
+    printf("\n");
 }
