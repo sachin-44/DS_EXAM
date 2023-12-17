f11e6bafcfb344b2f5966c371b89ac56cac3eded
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-12-17 13:45:08 +0530

    Create linkedpoly.c

---
+++ linkedpoly.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure for a term in a polynomial
+struct Node {
+    int coefficient;
+    int exponent;
+    struct Node *next;
+};
+
+// Function prototypes
+struct Node *createNode(int coefficient, int exponent);
+struct Node *insertTerm(struct Node *head, int coefficient, int exponent);
+struct Node *multiplyPolynomials(struct Node *poly1, struct Node *poly2);
+struct Node *addPolynomials(struct Node *poly1, struct Node *poly2);
+void displayPolynomial(struct Node *head);
+
+int main() {
+    struct Node *poly1 = NULL, *poly2 = NULL, *resultProduct = NULL, *resultSum = NULL;
+    int coeff, exp, n;
+
+    // Input for the first polynomial
+    printf("Enter the number of terms in the first polynomial: ");
+    scanf("%d", &n);
+    printf("Enter the terms (coefficient and exponent) for the first polynomial:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &coeff, &exp);
+        poly1 = insertTerm(poly1, coeff, exp);
+    }
+
+    // Input for the second polynomial
+    printf("Enter the number of terms in the second polynomial: ");
+    scanf("%d", &n);
+    printf("Enter the terms (coefficient and exponent) for the second polynomial:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &coeff, &exp);
+        poly2 = insertTerm(poly2, coeff, exp);
+    }
+
+    // Multiply polynomials and display result
+    resultProduct = multiplyPolynomials(poly1, poly2);
+    printf("\nProduct of the two polynomials:\n");
+    displayPolynomial(resultProduct);
+
+    // Add polynomials and display results
+    resultSum = addPolynomials(poly1, poly2);
+    printf("\nSum of the two polynomials:\n");
+    printf("First Polynomial: ");
+    displayPolynomial(poly1);
+    printf("Second Polynomial: ");
+    displayPolynomial(poly2);
+    printf("Resultant Polynomial: ");
+    displayPolynomial(resultSum);
+
+    // Free memory
+    free(poly1);
+    free(poly2);
+    free(resultProduct);
+    free(resultSum);
+
+    return 0;
+}
+
+// Function to create a node for a term in a polynomial
+struct Node *createNode(int coefficient, int exponent) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (!newNode) {
+        printf("Memory allocation error. Exiting.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->coefficient = coefficient;
+    newNode->exponent = exponent;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a term into a polynomial
+struct Node *insertTerm(struct Node *head, int coefficient, int exponent) {
+    struct Node *newNode = createNode(coefficient, exponent);
+
+    if (head == NULL) {
+        return newNode;
+    }
+
+    struct Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    return head;
+}
+
+// Function to multiply two polynomials
+struct Node *multiplyPolynomials(struct Node *poly1, struct Node *poly2) {
+    struct Node *result = NULL;
+
+    while (poly1 != NULL) {
+        struct Node *temp = poly2;
+        while (temp != NULL) {
+            int coeff = poly1->coefficient * temp->coefficient;
+            int exp = poly1->exponent + temp->exponent;
+            result = insertTerm(result, coeff, exp);
+            temp = temp->next;
+        }
+        poly1 = poly1->next;
+    }
+
+    return result;
+}
+
+// Function to add two polynomials
+struct Node *addPolynomials(struct Node *poly1, struct Node *poly2) {
+    struct Node *result = NULL;
+
+    while (poly1 != NULL && poly2 != NULL) {
+        if (poly1->exponent > poly2->exponent) {
+            result = insertTerm(result, poly1->coefficient, poly1->exponent);
+            poly1 = poly1->next;
+        } else if (poly1->exponent < poly2->exponent) {
+            result = insertTerm(result, poly2->coefficient, poly2->exponent);
+            poly2 = poly2->next;
+        } else {
+            int sum = poly1->coefficient + poly2->coefficient;
+            result = insertTerm(result, sum, poly1->exponent);
+            poly1 = poly1->next;
+            poly2 = poly2->next;
+        }
+    }
+
+    // Add remaining terms of poly1
+    while (poly1 != NULL) {
+        result = insertTerm(result, poly1->coefficient, poly1->exponent);
+        poly1 = poly1->next;
+    }
+
+    // Add remaining terms of poly2
+    while (poly2 != NULL) {
+        result = insertTerm(result, poly2->coefficient, poly2->exponent);
+        poly2 = poly2->next;
+    }
+
+    return result;
+}
+
+// Function to display a polynomial
+void displayPolynomial(struct Node *head) {
+    if (head == NULL) {
+        printf("Polynomial is empty.\n");
+        return;
+    }
+
+    while (head != NULL) {
+        printf("%dx^%d", head->coefficient, head->exponent);
+        head = head->next;
+
+        if (head != NULL) {
+            printf(" + ");
+        }
+    }
+
+    printf("\n");
+}
